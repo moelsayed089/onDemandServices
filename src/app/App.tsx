d7c9a55db9d81645917cafa4ab69105123a45e5d
moelsayed011 <me6758478@gmail.com>
2025-05-24 18:39:31 +0300

    start project

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-heading-1 font-bold underline bg-main-color">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
