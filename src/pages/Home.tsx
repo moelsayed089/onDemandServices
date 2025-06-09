5340118ffae9c4443cb1d6edcca5b401b81e2695
moelsayed011 <me6758478@gmail.com>
2025-06-09 18:32:38 +0300

    feat:add Starter Section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "../shared/components/organisms/HeroSection";
 import PricingInfoSection from "../shared/components/organisms/PricingInfoSection";
 import Services from "../shared/components/organisms/Services";
+import StarterSection from "../shared/components/organisms/StarterSection";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <HeroSection />
       <Services />
       <PricingInfoSection />
+      <StarterSection />
     </>
   );
 };
---
+++ src/shared/components/organisms/StarterSection.tsx
@@ -0,0 +1,28 @@
+import Image from "../atoms/Image";
+import Startnow from "../../../assets/images/StarterSection/Frame.png";
+import { Button } from "../atoms/Button";
+import { Link } from "react-router-dom";
+
+const StarterSection = () => {
+  return (
+    <>
+      <section className=" h-[350px]  mt-10 flex flex-col items-center justify-center ">
+        <Image src={Startnow} alt="Startnow" className=" w-full mb-10" />
+        <h2 className="text-heading-4  leading-heading-4  md:text-heading-3 md:leading-heading-3  font-medium text-center mb-7">
+          Register now and start shipping today.
+        </h2>
+
+        <Link to="/start">
+          <Button
+            variant="default"
+            className="py-5 w-[200px] mx-auto !text-white text-body-md hover:cursor-pointer"
+          >
+            Start Now
+          </Button>
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default StarterSection;
