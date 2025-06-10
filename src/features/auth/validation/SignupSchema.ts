0abab50000ced3edb1c7c058a6ef4ab92b1367aa
moelsayed011 <me6758478@gmail.com>
2025-06-10 19:08:22 +0300

    chore:edit validition password in sign up

--- src/features/auth/validation/SignupSchema.ts
+++ src/features/auth/validation/SignupSchema.ts
@@ -12,6 +12,9 @@ export const SignupSchema = Yup.object().shape({
 
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[0-9]/, "Password must contain at least one number")
     .required("Password is required"),
 
   cPassword: Yup.string()
