ac9053a910e8fc5b352b74f98f326f1fcc099aa8
moelsayed011 <me6758478@gmail.com>
2025-06-10 20:06:35 +0300

    chore:test refesh token

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,25 +1,9 @@
-import { useSelector } from "react-redux";
-import type { RootState } from "../app/store";
 import axiosInstance from "../app/api/axiosConfig";
 // import Cookies from "js-cookie";
 
 const Profile = () => {
-  const accessToken = useSelector(
-    (state: RootState) => state.loginAuth.accessToken
-  );
-  console.log(accessToken);
-
-  // const tokenFromCookie = Cookies.get("refreshToken");
-  // console.log("refresh Token from Cookie:", tokenFromCookie);
   const handleRefreshToken = async () => {
     try {
-      // const accessToken = Cookies.get("refreshToken");
-
-      // if (!accessToken) {
-      //   console.error("No refresh token found!");
-      //   return;
-      // }
-
       const response = await axiosInstance.post("/api/v1/auth/refresh-token", {
         withCredentials: true,
       });
