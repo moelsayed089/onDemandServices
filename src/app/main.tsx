fa0a592cf8642355e5c75f699bc1007e253f9f58
moelsayed011 <me6758478@gmail.com>
2025-06-10 20:16:45 +0300

    chore:test refesh token

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,5 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "../index.css";
+import TokenRefresher from "../pages/Profile";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <TokenRefresher />
+    <App />
+  </>
+);
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,24 +1,41 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import axiosInstance from "../app/api/axiosConfig";
-// import Cookies from "js-cookie";
+import { updateToken } from "../features/auth/authSlice";
 
-const Profile = () => {
+const TokenRefresher = () => {
+  const dispatch = useDispatch();
+
   const handleRefreshToken = async () => {
     try {
-      const response = await axiosInstance.post("/api/v1/auth/refresh-token", {
-        withCredentials: true,
-      });
+      const response = await axiosInstance.post(
+        "/api/v1/auth/refresh-token",
+        null,
+        {
+          withCredentials: true,
+        }
+      );
 
-      console.log(" Token refreshed:", response.data);
+      const { accessToken, accessTokenExpires } = response.data;
+
+      console.log("Token refreshed:", response.data);
+
+      // Update the token in Redux
+      dispatch(updateToken({ accessToken, accessTokenExpires }));
     } catch (error) {
       console.error("Error refreshing token:", error);
     }
   };
 
-  return (
-    <div>
-      <button onClick={handleRefreshToken}>Refresh Token</button>
-    </div>
-  );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleRefreshToken();
+    }, 60000); // 60000ms = 1 دقيقة
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  return null; // هذا الكمبوننت فقط للتحديث، مش بيعرض حاجة
 };
 
-export default Profile;
+export default TokenRefresher;
