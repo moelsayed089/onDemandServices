8be19a7383de4c9f50af0d015246bd2d0af031c9
moelsayed011 <me6758478@gmail.com>
2025-06-12 16:43:57 +0300

    chore:edit DropdownMenu component

--- src/shared/components/atoms/DropdownMenu.tsx
+++ src/shared/components/atoms/DropdownMenu.tsx
@@ -1,6 +1,22 @@
-import { DropdownMenu as ShadcnDropdownMenu } from "../ui/dropdown-menu";
-export const DropdownMenu = (
-  props: React.ComponentProps<typeof ShadcnDropdownMenu>
-) => {
-  return <ShadcnDropdownMenu {...props} />;
+import {
+  DropdownMenu as ShadcnDropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "../ui/dropdown-menu";
+
+type DropdownMenuType = typeof ShadcnDropdownMenu & {
+  Trigger: typeof DropdownMenuTrigger;
+  Content: typeof DropdownMenuContent;
+  Item: typeof DropdownMenuItem;
 };
+
+const DropdownMenu = ((props) => {
+  return <ShadcnDropdownMenu {...props} />;
+}) as DropdownMenuType;
+
+DropdownMenu.Trigger = DropdownMenuTrigger;
+DropdownMenu.Content = DropdownMenuContent;
+DropdownMenu.Item = DropdownMenuItem;
+
+export { DropdownMenu };
