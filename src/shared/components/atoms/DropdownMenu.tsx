ee1596ed33d8f02e7839e8369039d9e172a2a040
moelsayed011 <me6758478@gmail.com>
2025-06-12 15:11:32 +0300

    chore:add DropdownMenu component

---
+++ src/shared/components/atoms/DropdownMenu.tsx
@@ -0,0 +1,6 @@
+import { DropdownMenu as ShadcnDropdownMenu } from "../ui/dropdown-menu";
+export const DropdownMenu = (
+  props: React.ComponentProps<typeof ShadcnDropdownMenu>
+) => {
+  return <ShadcnDropdownMenu {...props} />;
+};
