3af61ef84a318d1599164757091004eec1c18d00
moelsayed011 <me6758478@gmail.com>
2025-06-16 17:02:39 +0300

    chore:add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      filename: "bundle-analysis.html",
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
