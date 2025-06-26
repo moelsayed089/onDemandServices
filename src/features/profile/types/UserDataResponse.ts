d563ef3efd90c28b4eda9264b6a41415943d11e9
moelsayed011 <me6758478@gmail.com>
2025-06-26 15:42:55 +0300

    feat(profile): add types for user data

---
+++ src/features/profile/types/UserDataResponse.ts
@@ -0,0 +1,10 @@
+export interface UserDataResponse {
+  data: {
+    name: string;
+    email: string;
+    phone: string;
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
