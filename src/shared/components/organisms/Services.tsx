504f5ee8bf5f46885261ca916d645377033b52a1
moelsayed011 <me6758478@gmail.com>
2025-11-11 13:33:55 +0200

    chore:edit businesses

--- src/shared/components/organisms/Services.tsx
+++ src/shared/components/organisms/Services.tsx
@@ -6,7 +6,7 @@ const Services = () => {
     <>
       <section className="container min-h-screen mt-16  ">
         <h2 className="text-heading-4  leading-heading-4  md:text-heading-3 md:leading-heading-3 lg:text-heading-2 lg:leading-heading-2 font-medium text-center mb-12">
-          Empowering businesses through DeliverCo services and solutions.
+          Empowering businesses DeliverCo services and solutions.
         </h2>
 
         {serviceData.map(({ icon, title, description, image, reverse }) => (
